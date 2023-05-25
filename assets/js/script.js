26bfb9198916c0910a662e41f23f0f09bc4ea090
Kyung Kwon <kyungkwon01@gmail.com>
2023-05-25 11:28:17 -0600

    second commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,21 @@
-import config from './config.js';
+const API_KEY = 'e8d973b8fba73a98a71fa8c57b8adc66'
+const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=${API_KEY}`
 
-const weatherApiKey = config.weatherApiKey
+function getApi() {
+    fetch(apiUrl)
+    .then(function(response) {
+        return response.json
+    })
+}
+
+
+fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      console.log('API Response:', data);
+     
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
 
