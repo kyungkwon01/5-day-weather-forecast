5ff3d59135c5dfc012f1023dd5aeee01b995a3be
Kyung Kwon <kyungkwon01@gmail.com>
2023-05-25 21:16:47 -0600

    update comments on js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,7 +54,7 @@ function initPage() {
               forecastDateEl.setAttribute("class", "mt-3 mb-0 forecast-date");
               forecastDateEl.innerHTML = forecastMonth + "/" + forecastDay + "/" + forecastYear;
               forecastEls[i].append(forecastDateEl);
-              // Icon for current weather
+              // Icon images for current weather
               const forecastWeatherEl = document.createElement("img");
               forecastWeatherEl.setAttribute("src", "https://openweathermap.org/img/wn/" + response.data.list[forecastIndex].weather[0].icon + "@2x.png");
               forecastWeatherEl.setAttribute("alt", response.data.list[forecastIndex].weather[0].description);
